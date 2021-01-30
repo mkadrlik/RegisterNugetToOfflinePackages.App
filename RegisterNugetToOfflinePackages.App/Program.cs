7b6263d713bf79f2895b5bf60969be933de68a2b
Matt Kadrlik <mkadrlik@gmail.com>
2021-01-30 11:52:33 -0600

    Code functional; issue with directory paths not resolving on Process.Start()

--- RegisterNugetToOfflinePackages.App/Program.cs
+++ RegisterNugetToOfflinePackages.App/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Configuration;
 using System.Diagnostics;
 using System.IO;
@@ -14,37 +15,66 @@ namespace RegisterNugetToOfflinePackages.App
             string folderPath = args[0];
 
             List<string> nugetDirectories = Directory.GetDirectories(folderPath).ToList();
+            StringBuilder fullLog = new StringBuilder();
 
             foreach (string nugetDirectory in nugetDirectories)
             {
                 List<string> nugetFile = Directory.GetFiles(nugetDirectory, "*.nupkg").ToList();
-                StringBuilder processingLog = new StringBuilder();
-                char[] buffer = null;
+                StringBuilder sb = new StringBuilder();
 
                 try
                 {
-                    Process process = Process.Start(ConfigurationManager.AppSettings["NugetExePath"]);
+                    string nugetStore = Path.Combine(Environment.GetEnvironmentVariable("ProgramFiles(x86)"), ConfigurationManager.AppSettings["NuGetStore"]);
+                    string nugetExePath = string.Concat(Path.Combine(Environment.GetEnvironmentVariable("ProgramFiles(x86)"), ConfigurationManager.AppSettings["NuGetExePath"]), " add ", nugetDirectory, " -Source ", nugetStore);
 
-                    StreamReader inputReader = new StreamReader(process.StandardInput.BaseStream);
-                    int bufferCount = inputReader.ReadAsync(buffer, 0, 0).Result;
-                    if (bufferCount == 0)
+                    Process process = new Process
                     {
-                        inputReader.Close();
-                        _ = processingLog.Append(buffer).AppendLine();
+                        StartInfo = new ProcessStartInfo
+                        {
+                            //Arguments = string.Concat(" add ", nugetDirectory, " -Source ", nugetStore),
+                            CreateNoWindow = false,
+                            FileName = nugetExePath,
+                            WindowStyle = ProcessWindowStyle.Hidden,
+                            RedirectStandardOutput = true,
+                            RedirectStandardError = true,
+                            RedirectStandardInput = true,
+                            UseShellExecute = false
+                        }
+                    };
+
+                    using (process)
+                    {
+                        Console.WriteLine(process.StartInfo.FileName);
+
+                        bool isStarted = process.Start();
+
+                        if (isStarted)
+                        {
+                            process.WaitForExit();
+
+                            _ = sb.Append(nugetFile[0].ToString()).AppendLine();
+                            if (process.StandardInput.BaseStream.CanRead) _ = sb.Append(process.ExitCode).AppendLine();
+                            if (process.StandardOutput.BaseStream.CanRead) _ = sb.Append(process.StandardOutput.ReadToEnd()).AppendLine();
+                            if (process.StandardError.BaseStream.CanRead) _ = sb.Append(process.StandardError.ReadToEnd()).AppendLine();
+                        }
                     }
-
-                    StreamReader outputReader = new StreamReader(process.StandardOutput.BaseStream);
-                    processingLog = outputReader.ReadToEnd();
-                    reader.Close();
-
-                    StreamReader errorReader = new StreamReader(process.StandardError.BaseStream);
-                    processingLog = errorReader.ReadToEnd();
-                    reader.Close();
                 }
-                catch (Exception)
+                catch (Exception ex)
                 {
+                    _ = sb.Append(ex.Message).AppendLine();
+                    _ = sb.Append(ex.StackTrace).AppendLine();
+                    while (ex.InnerException != null && !string.IsNullOrWhiteSpace(ex.InnerException.Message))
+                    {
+                        _ = sb.Append(ex.InnerException.Message).AppendLine();
+                    }
                 }
+
+                _ = sb.AppendLine();
+                _ = fullLog.Append(sb);
+                Console.WriteLine(sb.ToString());
             }
+
+            File.WriteAllText(string.Concat(Environment.CurrentDirectory, "ProcessingLog", "_", DateTime.Now.Ticks, ".txt"), fullLog.ToString());
         }
     }
 }
